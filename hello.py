530f6824b9ebdde03a9ab1671c17098f62ddde3c
Andrew Barisser <barisser@gmail.com>
2014-07-21 22:21:57 -0700

    python server

--- hello.py
+++ hello.py
@@ -4,8 +4,21 @@ import requests
 
 app = Flask(__name__)
 
+def blockjson(blockn):
+  a=requests.get('http://blockchain.info/block-index'+str(blockn)+'?format=json')
+  b=a.content
+  return str(b)
+
+
 @app.route('/')
 def hello():
     a=requests.get('http://blockchain.info/block-index/0?format=json')
     b=a.content
     return str(b)
+
+
+@app.route('/', methods=['POST'])
+def hello():
+    blockn=request.form['block_height']
+    #email=request.form['youremail']
+    return blockjson(blockn)
